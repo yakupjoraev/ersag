06ab5ebc32b66c026a811aa46bbc6b1fa8323475
Yakup <yakupjoraev@gmail.com>
2022-09-21 23:21:01 +0300

    section reviews

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,3 +1,17 @@
 // Custom Scripts
 // Custom scripts
-
+// Initialize Swiper
+  var reviewsSwiper = new Swiper(".reviews__slider-container", {
+    cssMode: true,
+    // centeredSlides,
+    // slidesPerView: 1,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,16 @@
 // Custom scripts
+// Initialize Swiper
+  var reviewsSwiper = new Swiper(".reviews__slider-container", {
+    cssMode: true,
+    // centeredSlides,
+    // slidesPerView: 1,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
