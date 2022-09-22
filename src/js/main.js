b7e242f0ac7db801870be7357ea7dbf3a29a3619
Yakup <yakupjoraev@gmail.com>
2022-09-22 11:17:48 +0300

    fix some elements

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,18 @@
 // Custom scripts
+// Вызываем эту функцию, если нам нужно зафиксировать меню при скролле.
+function fixedNav() {
+  const nav = document.querySelector('nav')
+
+  // тут указываем в пикселях, сколько нужно проскроллить что бы наше меню стало фиксированным
+  const breakpoint = 1
+  if (window.scrollY >= breakpoint) {
+    nav.classList.add('fixed__nav')
+  } else {
+    nav.classList.remove('fixed__nav')
+  }
+}
+window.addEventListener('scroll', fixedNav)
+
 // Initialize Swiper
   var reviewsSwiper = new Swiper(".reviews__slider-container", {
     cssMode: true,
